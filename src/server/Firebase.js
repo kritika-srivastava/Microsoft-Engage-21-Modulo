79a718ad31f6b78e13c82f45f3c305fed45c819e
kritika-srivastava <kritika.s1408@gmail.com>
2021-11-21 23:32:15 +0530

    Firebase Initialization

---
+++ src/server/Firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/database';
+
+var firebaseConfig = {
+
+    apiKey: "AIzaSyBQcSjhRX900fOAI0of9MCNAOKVVQkk4mM",
+    authDomain: "modulo-dd1f4.firebaseapp.com",
+    databaseURL: "https://modulo-dd1f4-default-rtdb.firebaseio.com",
+    projectId: "modulo-dd1f4",
+    storageBucket: "modulo-dd1f4.appspot.com",
+    messagingSenderId: "299719871334",
+    appId: "1:299719871334:web:6d80c6c6774d30c6c8d6a7",
+    measurementId: "G-BL1GG8DTKH"
+
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+firebase.Analytics();
+
+export default firebase;
